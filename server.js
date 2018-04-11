40ca4d58a68f0e8fd4438f1f9e4b2edf7928ebc8
Matt Armstrong-Totten <matt.armstro@gmail.com>
2018-04-11 07:52:24 -0400

    added cors functionality

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+import { appendFileSync } from 'fs';
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 
 const schema = require('./data/schema');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
